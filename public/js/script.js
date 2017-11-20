7961b03020662e3811f5ccde38077a2869e7a1c7
Will <wjbeshore@gmail.com>
2017-11-20 09:27:26 -0600

    styling changes

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,29 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+var i = 0;
+var o = 0;
+var txt1 = 'WILL BESHORE WEB DEVELOPER';
+var txt2= "Beshore";
+var speed = 200;
+
+function typeWriter() {
+  if (i < txt1.length) {
+    document.getElementById("lead").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function toggle_visibility() {
+      var e = document.getElementById("cursor");
+      if(e.style.display == 'block')
+         e.style.display = 'none';
+      else
+         e.style.display = 'block';
+
+       }
+
+  var t=setInterval(toggle_visibility,500);
+  t();
