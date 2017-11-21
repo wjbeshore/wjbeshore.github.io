e69be939732a5c6bc413453a049dfb1871b57a46
Will <wjbeshore@gmail.com>
2017-11-21 12:37:23 -0600

    styling work, added pics

--- public/js/script.js
+++ public/js/script.js
@@ -8,28 +8,52 @@ function myFunction() {
     }
 }
 
+//Hi, I'm
+//Will BESHORE
+//this is my
+//portfolio
+
 var i = 0;
-var o = 0;
-var txt1 = 'WILL BESHORE WEB DEVELOPER';
-var txt2= "Beshore";
-var speed = 200;
+var i2 = 0;
+var i3 = 0;
+var i4 = 0;
+
+var txt1 = "Hi, I'm";
+var txt2= "Will Beshore";
+var txt3= "this is my";
+var txt4= "Portfolio"
+var speed = 150;
 
 function typeWriter() {
   if (i < txt1.length) {
-    document.getElementById("lead").innerHTML += txt1.charAt(i);
+    document.getElementById("lead1").innerHTML += txt1.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
 
-function toggle_visibility() {
-      var e = document.getElementById("cursor");
-      if(e.style.display == 'block')
-         e.style.display = 'none';
-      else
-         e.style.display = 'block';
+function typeWriter2() {
+  if (i < txt2.length) {
+    document.getElementById("lead2").innerHTML += txt2.charAt(i2);
+    i2++;
+    setTimeout(typeWriter2, speed);
+  }
+}
+function typeWriter3() {
+  if (i < txt3.length) {
+    document.getElementById("lead3").innerHTML += txt3.charAt(i3);
+    i3++;
+    setTimeout(typeWriter3, speed);
+  }
+}
+function typeWriter4() {
+  if (i < txt4.length) {
+    document.getElementById("lead4").innerHTML += txt4.charAt(i4);
+    i4++;
+    setTimeout(typeWriter4, speed);
+  }
+}
 
-       }
-
-  var t=setInterval(toggle_visibility,500);
-  t();
+function addActive() {
+  document.getElementById("lead4").className += ' activate';
+}
